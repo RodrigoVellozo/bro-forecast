01bfbc5f59114bea9e7b244bb44fe913ce127284
Rodrigo Vellozo <rodrigovellozosi@gmail.com>
2024-01-22 11:19:50 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-climatempo';
+  lat: number = 0;
+  long: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  public getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+      });
+    } else {
+      alert('Foi nao');
+    }
+  }
 }
